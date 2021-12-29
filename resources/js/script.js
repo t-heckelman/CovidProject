abb58c7c84fe02240fbfd806e8e14cb39db1f670
t-heckelman <theodore.heckelman@colorado.edu>
2021-12-28 21:58:37 -0500

    nice

--- resources/js/script.js
+++ resources/js/script.js
@@ -107,30 +107,3 @@ function enableButton(letter, capital, number, length, match, email) {
     button.disabled = true;
   }
 }
-const searchFocus = document.getElementById('search-focus');
-const keys = [
-{ keyCode: 'AltLeft', isTriggered: false },
-{ keyCode: 'ControlLeft', isTriggered: false },
-];
-
-window.addEventListener('keydown', (e) => {
-  keys.forEach((obj) => {
-    if (obj.keyCode === e.code) {
-      obj.isTriggered = true;
-    }
-  });
-
-  const shortcutTriggered = keys.filter((obj) => obj.isTriggered).length === keys.length;
-
-  if (shortcutTriggered) {
-    searchFocus.focus();
-  }
-});
-
-window.addEventListener('keyup', (e) => {
-  keys.forEach((obj) => {
-    if (obj.keyCode === e.code) {
-      obj.isTriggered = false;
-    }
-  });
-});
