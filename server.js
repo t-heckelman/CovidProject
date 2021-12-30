260b4f846b88e8e41f620fe9d2e02948f6a855f1
t-heckelman <theodore.heckelman@colorado.edu>
2021-12-30 00:38:58 -0500

    reviews page

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ let dbConfig = {
   host: "127.0.0.1",
   port: 5432,
   database: "postgres",
-  user: "malcolmholman",
+  user: "teddyheckelman",
   password: "password",
 };
 
@@ -62,8 +62,8 @@ app.get("/logout", function(req,res) {
     my_title: "Music Space",
     dailyImg: dailyImg,
     user: user,
-    error: false,  
-  });  
+    error: false,
+  });
 });
 
 
